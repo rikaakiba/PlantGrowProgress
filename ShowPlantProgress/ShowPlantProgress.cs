d481299876dd6bc84e6404b8f7bc9748b1137887
Akiba Rika <perfectt93@gmail.com>
2024-04-29 19:10:56 +0900

    Fix for Ashland update

--- ShowPlantProgress/ShowPlantProgress.cs
+++ ShowPlantProgress/ShowPlantProgress.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace ShowPlantProgress
 {
-    [BepInPlugin("smallo.mods.showplantprogress", "Show Plant Progress", "1.3.0")]
+    [BepInPlugin("smallo.mods.showplantprogress", "Show Plant Progress", "1.4.0")]
     [HarmonyPatch]
     class ShowPlantProgressPlugin : BaseUnityPlugin
     {
@@ -32,7 +32,7 @@ namespace ShowPlantProgress
             string colour = "red";
             if (percentage >= 25 && percentage <= 49) colour = "orange";
             if (percentage >= 50 && percentage <= 74) colour = "yellow";
-            if (percentage >= 75 && percentage <= 100) colour = "lime";
+            if (percentage >= 75 && percentage <= 100) colour = "green";
 
             return colour;
         }
@@ -43,7 +43,7 @@ namespace ShowPlantProgress
         {
             if (__instance == null) return __result;
 
-            double percentage = Mathf.Floor((float)__instance.TimeSincePlanted() / (float)__instance.GetGrowTime() * 100);
+            double percentage = Mathf.Floor((float)Traverse.Create(__instance).Method("TimeSincePlanted").GetValue<double>() / Traverse.Create(__instance).Method("GetGrowTime").GetValue<float>() * 100);
             string colour = GetColour(percentage);
             string growPercentage = $"<color={colour}>{decimal.Round((decimal)percentage, amountofDecimals.Value, MidpointRounding.AwayFromZero)}%</color>";
 
@@ -52,46 +52,18 @@ namespace ShowPlantProgress
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(Pickable), "GetHoverText")]
-        public static string BerryBushPickable_Patch(string __result, Pickable __instance)
+        public static string BerryBushPickable_Patch(string __result, Pickable __instance, ZNetView ___m_nview)
         {
             if (!bushList.Contains(__instance.name)) return __result;
 
-            DateTime startTime = new DateTime(__instance.m_nview.GetZDO().GetLong("picked_time"));
-            double percentage = (ZNet.instance.GetTime() - startTime).TotalMinutes / __instance.m_respawnTimeMinutes * 100;
+            DateTime startTime = new DateTime(___m_nview.GetZDO().GetLong(ZDOVars.s_pickedTime, 0L));
+            double percentage = (ZNet.instance.GetTime() - startTime).TotalMinutes / (double)__instance.m_respawnTimeMinutes * 100;
             if (percentage > 99.99f) return __result;
 
             string colour = GetColour(percentage);
             string growPercentage = $"<color={colour}>{decimal.Round((decimal)percentage, amountofDecimals.Value, MidpointRounding.AwayFromZero)}%</color>";
 
             return __result + $"{Localization.instance.Localize(__instance.GetHoverName())} ( {growPercentage} )";
-        }
-
-        // Berry bush time reset fix thanks to `3d_printing_dm` from https://old.reddit.com/r/valheim/comments/lzcxr3/bug_in_berry_bush_pick_time_code/
-        [HarmonyPatch(typeof(Pickable), "SetPicked")]
-        public static class FixPickableTime
-        {
-            public class PickState
-            {
-                public long picked_time;
-                public bool picked;
-            }
-
-            [HarmonyPrefix]
-            public static void Prefix(bool picked, ZNetView ___m_nview, bool ___m_picked, ref PickState __state)
-            {
-                __state = new PickState();
-                __state.picked_time = ___m_nview.GetZDO().GetLong("picked_time", 0L);
-                __state.picked = ___m_picked;
-            }
-
-            [HarmonyPostfix]
-            public static void Postfix(bool picked, ZNetView ___m_nview, bool ___m_picked, ref PickState __state)
-            {
-                if (__state.picked == ___m_picked)
-                {
-                    ___m_nview.GetZDO().Set("picked_time", __state.picked_time);
-                }
-            }
         }
     }
 }
